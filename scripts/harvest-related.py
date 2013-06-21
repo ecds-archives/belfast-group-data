fba8ca0a52b5b59bc8ab5f4322ee7fdb487d7f4a
rlskoeser <rebecca.s.koeser@emory.edu>
2013-06-21 12:35:13 -0400

    update to use requests with content-negotation to get non-empty RDF for dbpedia and avoid parse errors; add progressbar support

--- scripts/harvest-related.py
+++ scripts/harvest-related.py
@@ -3,15 +3,24 @@
 # pip deps
 # rdflib
 
-import os
 import argparse
+import os
+import sys
 import urllib2
 
 try:
     import rdflib
+    import requests
 except ImportError:
-    print '''Please install rdflib (pip install or easy_install rdflib)'''
+    print '''Please install rdflib and requests (pip install or easy_install rdflib requests)'''
     exit(-1)
+
+
+try:
+    from progressbar import ProgressBar, Bar, Percentage, ETA, SimpleProgress, Timer
+except ImportError:
+    ProgressBar = None
+    print '''Optionally install progressbar (pip install or easy_install progressbar)'''
 
 
 def process_files(files, basedir):
@@ -24,19 +33,24 @@ def process_files(files, basedir):
             print "Error parsing '%s' as RDF -- %s" % (infile, err)
             continue
 
-    resources = {
-        'viaf': 'http://viaf.org/',
-        'dbpedia': 'http://dbpedia.org/',
-        'geonames': 'http://sws.geonames.org/',
-        # possibly use a regex for geonames variants?
-    }
+    resources = [
+        # NOTE: using tuples to ensure we process in this order,
+        # to allow harvesting dbpedia records referenced in viaf/geonames
+        ('viaf', 'http://viaf.org/'),
+        ('geonames', 'http://sws.geonames.org/'),
+        ('dbpedia', 'http://dbpedia.org/'),
+    ]
 
-    for name, url in resources.iteritems():
+    for name, url in resources:
+        # anything that is a subject or object and has a
+        # viaf, dbpedia, or geoname uri
         res = g.query('''
-            SELECT DISTINCT ?s
+            SELECT DISTINCT ?uri
             WHERE {
-            ?s ?p ?o .
-            FILTER regex(str(?s), "^%s") .
+            {?uri ?p ?o }
+            UNION
+            {?s ?p ?uri }
+            FILTER regex(str(?uri), "^%s") .
             }
             ''' % url)
         print '%d %s URI%s' % (len(res), name,
@@ -45,27 +59,43 @@ def process_files(files, basedir):
         if len(res) == 0:
             continue
 
-        uris = [str(r['s']) for r in res]
+        uris = [str(r['uri']) for r in res]
 
         datadir = os.path.join(basedir, name)
         if not os.path.isdir(datadir):
             os.mkdir(datadir)
 
-        # TODO: progressbar would be nice
+        if len(uris) >= 5 and ProgressBar and os.isatty(sys.stderr.fileno()):
+            widgets = [Percentage(), ' (', SimpleProgress(), ')',
+                       Bar(), ETA()]
+            progress = ProgressBar(widgets=widgets, maxval=len(uris)).start()
+            processed = 0
+        else:
+            progress = None
+
         for u in uris:
-            # build filename brased on viaf id
+            # build filename based on viaf id
             baseid = u.rstrip('/').split('/')[-1]
             filename = os.path.join(datadir, '%s.rdf' % baseid)
-            # Use rdflib to load the data - should handle content negotation, etc
-            data = rdflib.Graph()
-            try:
-                data.parse(u)
-            except urllib2.HTTPError:
-                print 'Error loading %s' % u
-                continue
 
-            with open(filename, 'w') as datafile:
-                data.serialize(datafile)
+            # Use requests with content negotiation to load the data
+            data = requests.get(u, headers={'accept': 'application/rdf+xml'})
+            if data.status_code == requests.codes.ok:
+                # also add to master graph so we can download related data
+                # i.e.  dbpedia records for VIAF persons
+                g.parse(data=data.content)
+
+                with open(filename, 'w') as datafile:
+                    datafile.write(data.content)
+            else:
+                print 'Error loading %s : %s' % (u, data.status_code)
+
+            if progress:
+                processed += 1
+                progress.update(processed)
+
+        if progress:
+            progress.finish()
 
 
 if __name__ == '__main__':
