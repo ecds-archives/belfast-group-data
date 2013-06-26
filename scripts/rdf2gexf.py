dc6936ead8cc96f4781b6cdf58b5e1c4937ae650
rlskoeser <rebecca.s.koeser@emory.edu>
2013-06-26 12:14:26 -0400

    revise rdf2gexf script to take input/output args so it can be used for any rdf dataset

--- scripts/rdf2gexf.py
+++ scripts/rdf2gexf.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import glob
 import json
 import networkx as nx
@@ -30,55 +31,67 @@ def get_best_label(res, graph):
         ns, short_type = rdflib.namespace.split_uri(type)
         return short_type
 
-# load all the data into a single rdf graph
-g = rdflib.Graph()
-files = 0
-for infile in glob.iglob('data/*.xml'):
-    g.parse(infile)
-    files +=1
 
-print '%d triples in %d files' % (len(g), files)
+def process_files(files, outfile):
+    # load all the data into a single rdf graph
+    g = rdflib.Graph()
+    for infile in files:
+        g.parse(infile)
 
-nxg = nx.MultiDiGraph()
+    print '%d triples in %d files' % (len(g), len(files))
 
+    nxg = nx.MultiDiGraph()
 
-# iterate through rdf triples and add to the graph
-for subj, pred, obj in g:
-    # make sure subject and object (if a resource) are added to the graph as nodes
-    if isinstance(subj, rdflib.URIRef) or isinstance(subj, rdflib.BNode) \
-          and subj not in nxg:
-        add_opts = {}
-        label = get_best_label(subj, g)
-        if label is not None:
-            add_opts['label'] = label
-            nxg.add_node(subj, **add_opts)
+    # iterate through rdf triples and add to the graph
+    for subj, pred, obj in g:
+        # make sure subject and object (if a resource) are added to the graph as nodes
+        if isinstance(subj, rdflib.URIRef) or isinstance(subj, rdflib.BNode) \
+           and subj not in nxg:
+            add_opts = {}
+            label = get_best_label(subj, g)
+            if label is not None:
+                add_opts['label'] = label
+                nxg.add_node(subj, **add_opts)
 
-    if pred is not rdflib.RDF.type and \
-            (isinstance(obj, rdflib.URIRef) or isinstance(obj, rdflib.BNode)) \
-            and obj not in nxg:
-        add_opts = {}
-        label = get_best_label(obj, g)
-        if label is not None:
-           add_opts['label'] = label
-        nxg.add_node(obj, **add_opts)
+        if pred is not rdflib.RDF.type and \
+           (isinstance(obj, rdflib.URIRef) or isinstance(obj, rdflib.BNode)) \
+           and obj not in nxg:
+            add_opts = {}
+            label = get_best_label(obj, g)
+            if label is not None:
+             add_opts['label'] = label
+             nxg.add_node(obj, **add_opts)
 
-    # get the short-hand name for property or edge label
-    ns, name = rdflib.namespace.split_uri(pred)
+        # get the short-hand name for property or edge label
+        ns, name = rdflib.namespace.split_uri(pred)
 
-    # if the object is a literal, add it to the node as a property of the subject
-    if subj in nxg and isinstance(obj, rdflib.Literal) or pred == rdflib.RDF.type:
-        if pred == rdflib.RDF.type:
-            ns, val = rdflib.namespace.split_uri(obj)
+        # if the object is a literal, add it to the node as a property of the subject
+        if subj in nxg and isinstance(obj, rdflib.Literal) or pred == rdflib.RDF.type:
+            if pred == rdflib.RDF.type:
+                ns, val = rdflib.namespace.split_uri(obj)
+            else:
+                val = unicode(obj)
+            nxg.node[subj][name] = val
+
+        # otherwise, add an edge between the two resource nodes
         else:
-            val = unicode(obj)
-        nxg.node[subj][name] = val
+            nxg.add_edge(subj, obj, label=name)
 
-    # otherwise, add an edge between the two resource nodes
-    else:
-        nxg.add_edge(subj, obj, label=name)
+    print '%d nodes, %d edges' % (nxg.number_of_nodes(), nxg.number_of_edges())
+    gexf.write_gexf(nxg, outfile)
 
 
-print '%d nodes, %d edges' % (nxg.number_of_nodes(), nxg.number_of_edges())
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Generate a network graph file in GEXF format based on RDF'
+    )
+    parser.add_argument('files', metavar='FILE', nargs='+',
+                        help='files to be processed')
+    parser.add_argument('-o', '--output', metavar='OUTFILE',
+                        help='filename for GEXF to be generated',
+                        required=True)
+    args = parser.parse_args()
+    process_files(args.files, args.output)
 
-gexf.write_gexf(nxg, 'belfast-group-data.gexf')
+
 
