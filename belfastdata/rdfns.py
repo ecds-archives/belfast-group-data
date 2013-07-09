38953c1b153d26ae5ad8ac043fd3f4d66b3be277
rlskoeser <rebecca.s.koeser@emory.edu>
2013-07-09 18:16:14 -0400

    add namespace for local belfast group ontology [#53096281]

--- belfastdata/rdfns.py
+++ belfastdata/rdfns.py
@@ -7,6 +7,8 @@ DC = rdflib.Namespace('http://purl.org/dc/terms/')
 DCMITYPE = rdflib.Namespace('http://purl.org/dc/dcmitype/')
 BIBO = rdflib.Namespace('http://purl.org/ontology/bibo/')
 
+# local belfast group ontology
+BG = rdflib.Namespace('http://belfastgroup.library.emory.edu/ontologies/2013/6/belfastgroup/#')
 
 # not technically a namespace, but seems sensible to keep here
 BELFAST_GROUP_URI = 'http://viaf.org/viaf/123393054'
